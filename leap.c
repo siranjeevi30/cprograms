e829edb60e71b20652a436b2193c372790e66560
siranjeevi30 <47448850+siranjeevi30@users.noreply.github.com>
2019-02-08 18:46:19 +0530

    Create leap.c

---
+++ leap.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(void) 
+{
+	int a;
+	scanf("%d",&a);
+	if(a%400==0)
+	{
+		printf("yes");
+	}
+	else if(a%100==0)
+	{
+		printf("yes");
+	}
+	else if(a%4==0)
+	{
+		printf("yes");
+	}
+	else
+	{
+		printf("no");
+	}
+	// your code goes here
+	return 0;
+	
+}
